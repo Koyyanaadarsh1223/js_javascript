725876c4157e66ec088db2c564b2355b9d29cf18
Koyyanaadarsh1223 <adarshadi2932@gmail.com>
2025-04-09 19:11:47 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 300442
+let accountEmail = "adarsh@email.com"
+var accountPassword = "1223"
+accountCity = "hyderabad"
+/* 
+we can declare without using const and var.
+const means we cannot update the variable value
+let means we can update and change the varable value
+console.log(x) which prints only x
+console.table([x,y,z]) which prints x,y,z
+"use strict"; this treat all JS code as newer version
+code readability should be insane
+learn all datatypes
+to find unique we use symbols
+learn about premitives 
+"typeof" this tell us their datatype 
+*/
